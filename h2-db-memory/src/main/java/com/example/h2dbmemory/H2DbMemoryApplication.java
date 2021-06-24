5b0550486efb03cce841e6c32c8c7e0e398d7147
Kradeyes <yanin.r.a@mail.ru>
2021-06-24 16:22:24 +0300

    Разработать микросервис содержащий CRUD REST API и работающий с in memory базой данных h2. Работа с репозиторием должна осуществляться из сервиса (специализированного класса с аннотацией @Service). В контроллере не должно быть dependency injection репозитория.

---
+++ h2-db-memory/src/main/java/com/example/h2dbmemory/H2DbMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2dbmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DbMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2DbMemoryApplication.class, args);
+    }
+
+}
